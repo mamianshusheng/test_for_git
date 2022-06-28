e4966b1a44f88f3a86805514178e12b5f97ab085
huakunxu9810@foxmail.com <huakunxu9810@foxmail.com>
2022-06-28 08:57:10 +0800

    the initial edition

---
+++ UDP_ServerDlg.h
@@ -0,0 +1,36 @@
+﻿
+// UDP_ServerDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CUDPServerDlg 对话框
+class CUDPServerDlg : public CDialogEx
+{
+// 构造
+public:
+	CUDPServerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_UDP_SERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+};
